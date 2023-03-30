c00b123ae9ca4ff3fe07ad11f82c6f849f18ffc2
Kirk Rohani <kirk.rohani@gmail.com>
2023-03-30 16:29:30 -0500

    fixed import statement, added state var for text on screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 
-import Component from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: " Your momma don't wear no draws! Class Component."
+    };
+  }
+
   render() {
       return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Your momma don't wear no draws.
-            </p>
+            <p>{this.state.text}</p>
+            <button>Change Text</button>
             <a
               className="App-link"
               href="https://reactjs.org"
