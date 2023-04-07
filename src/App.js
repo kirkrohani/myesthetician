94eca86f74fd2e9a71243ecdfe32748574ad49c8
Kirk Rohani <kirk.rohani@gmail.com>
2023-04-07 14:16:19 -0500

    added filterCustomers function, filteredCustomers state var

--- src/App.js
+++ src/App.js
@@ -4,42 +4,51 @@ import { Component } from 'react';
 
 class App extends Component {
   constructor() {
+    console.log('constructor');
     super();
     this.state = {
-      customers: []
+      customers: [],
+      filteredCustomers: []
     };
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(customers => this.setState(
-        () => { return { customers: customers } },
+        () => {
+          return {
+            customers: customers,
+            filteredCustomers: customers  }
+        },
         () => {
           console.log('Customers in State: ', this.state.customers)
         }))
       .catch(error => console.log('Handle this error: ', error));
   }
 
+  filterCustomers(event) {
+    const filteredCustomers = this.state.customers.filter(customer => customer.name.toLowerCase().includes(event.target.value));
+    this.setState(() => { return { filteredCustomers } });
+  }
+
+
+
   render() {
     console.log('rendering....');
-      return (
-        <div className="App">
-          <br />
-          <input onChange={
-            (evt) => {
-              const filteredCustomers = this.state.customers.filter(customer => customer.name.toLowerCase().includes(evt.target.value));
-
-              this.setState({customers: filteredCustomers});
-            }
-          }
-            className='search-box'
-            type='search'
-            placeholder='search customers'
-          />
-          {this.state.customers.map((customer, index) => <h1 key={index}>{customer.name}</h1>)}
-        </div>
-      );
+      
+    return (
+      <div className="App">
+        <br />
+        <input onChange={(evt) => { this.filterCustomers(evt) }}
+          className='search-box'
+          type='search'
+          placeholder='search customers'
+        />
+        {this.state.filteredCustomers.map((customer, index) => <h1 key={index}>{customer.name}</h1>)}
+      </div>
+    );
   }
 }
 
